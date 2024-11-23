52f035756d9399a731d593a0891420e85604df02
Bendy <thanhtungnguyennnn@gmail.com>
2024-11-23 15:53:49 +0700

    fdsfes

--- Assets/Script/Key/TreasureChest.cs
+++ Assets/Script/Key/TreasureChest.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class TreasureChest : MonoBehaviour
 {
-    
-    private bool isOpen = false;
+    public GameObject notification_Perfab;
+    private GameObject notification_Canvas;
 
+    private bool isOpen = false;
     private Animator chestAnimator;
     private void Start()
     {
@@ -19,6 +21,7 @@ public class TreasureChest : MonoBehaviour
             isOpen = true;
             KeyManager.instance.AddKey();
             OpenChest();
+            ShowNotification();
             Debug.Log("Nguoi choi da nhan duoc key tu vung dat");
             Destroy(gameObject, 2);
         }
@@ -28,6 +31,28 @@ public class TreasureChest : MonoBehaviour
         if(chestAnimator != null)
         {
             chestAnimator.SetTrigger("Openchest");
+        }
+    }
+    private void ShowNotification()
+    {
+        //khoi tao panel thong bao tu perfab
+        notification_Canvas = Instantiate(notification_Perfab);
+
+        //dam bao canvas ton tai va hien thi dung
+        if(notification_Canvas != null)
+        {
+            notification_Canvas.SetActive(true);//hien thi canvas
+            StartCoroutine(WaitForAnyToClose());//choi nguoi choi nhan nut bat ki
+        }
+    }
+    private IEnumerator WaitForAnyToClose()
+    {
+        //nguoi choi nhan phim bat ki
+        yield return new WaitUntil(() => Input.anyKeyDown);
+
+        if(notification_Canvas != null)
+        {
+            Destroy(notification_Canvas); //xoa canvas
         }
     }
 }
