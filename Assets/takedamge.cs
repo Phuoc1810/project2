8df14a22defb4d376fb6337cdd5c4e51277cca10
Phuoc1810 <phuoc1122181@gmail.com>
2024-12-02 20:37:35 -0300

    pp
    dd

--- Assets/takedamge.cs
+++ Assets/takedamge.cs
@@ -6,17 +6,26 @@ using UnityEngine;
 
 public class takedamge : MonoBehaviour
 {
+    private Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("fire"))
         {
+            
             beingattack();
 
         }
+       
     }
+  
     void beingattack()
     {
+        anim.SetTrigger("hurt");
         var firedamge = 20;
         var playerdef = player._instance.GetComponent<playersat>().defent;//chi so phong thu cua player
         var damage = firedamge - playerdef;//can bang game
