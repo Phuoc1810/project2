83558b5028d49953398114f6904c89334c65d03c
Phuoc1810 <phuoc1122181@gmail.com>
2024-12-04 09:58:03 -0300

    Â
    Â

--- Assets/Script/Player/move.cs
+++ Assets/Script/Player/move.cs
@@ -1,14 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class player : MonoBehaviour
 {
+    Animator ani;
+    bool trigger;
+    public int combo;
+    public int combonumber;
+    public bool attacking;
+    
+
+    public float combotiming;
+
+    public float combotempo;
     [SerializeField] private int rspeed;
     [SerializeField] private int speed;
     private Rigidbody2D rb;
-    private Animator anim;
+    Animator anim;
     public static player _instance;
 
     public string namescene;
@@ -24,6 +33,11 @@ public class player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        combonumber = 2;
+        
+        combo = 1;
+        combotiming = 1f;
+        combotempo = combotiming;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 
@@ -49,7 +63,7 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Combo();
         var moveX = Input.GetAxis("Horizontal");
         var moveY = Input.GetAxis("Vertical");
         //khuyen khich dung rigidbody de di chuyen
@@ -65,22 +79,7 @@ public class player : MonoBehaviour
             anim.SetFloat("moveX", moveX);
             anim.SetFloat("moveY", moveY);
         }
-        if(isattacking)
-        {
-            
-            attackcouter -= Time.deltaTime;
-            if(attackcouter<=0)
-            {
-                anim.SetBool("isattacking", false);
-                isattacking = false;
-            }
-        }
-        if(Input.GetKeyDown(KeyCode.J))
-        {
-            attackcouter = attacktime;
-            anim.SetBool("isattacking", true);
-            isattacking = true;
-        }
+       
        if(Input.GetKeyDown (KeyCode.E))
         {
             panel_UIPlayer.SetActive(!panel_UIPlayer.activeSelf);
@@ -93,5 +92,52 @@ public class player : MonoBehaviour
                 Time.timeScale = 1;
             }
         }
+    }
+    public void Combo()
+    {
+        //giam combo tempo theo thoi gian khung hinh time.deltatime;
+        combotempo -= Time.deltaTime;
+        //neu co lenh tan cong thi moi thuc hien combo
+        if (Input.GetKeyDown(KeyCode.J) && combotempo < 0)
+        {
+            //bat trang thai tan cong
+            attacking = true;
+            //kich hoat animation tan conng
+            
+            anim.SetTrigger("attack" + combo);
+            //combotempo=combotiming
+            combotempo = combotiming;
+        }
+        //neu chua het thoi gian de kich hoat combo
+        else if (Input.GetKeyDown(KeyCode.J) && combotempo > 0 && combotempo > 0.3)
+        {
+            // bat trang thai tan cong
+            attacking = true;
+
+            //tang gia tri bien dem compo ktr xem vuot bien dem chua
+            combo += 1;
+
+            //neu da dat gioi hang combo thi set combonumber=1
+            if (combo > combonumber)
+            {
+                combo = 1;
+            }
+            //show ra animation tan cong
+           
+            anim.SetTrigger("attack" + combo);
+            //thiet lap lai gia tri combotiming
+            combotempo = combotiming;
+        }
+        //neu nhu k co lenh tan cong nao dc thuc hien thi trang thai tan cong tat
+        else if (combotempo < 0 && !Input.GetKeyDown(KeyCode.J))
+        {
+            attacking = false;
+        }
+        //neu combo tempo <0 thi reset gia tri combo ve 1 de thuc hien combo
+        if (combotempo < 0)
+        {
+            combo = 1;
+        }
+
     }
 }
